5dc895946ca0c47bdc047c3d69032adef2bf41d4
clemay <clement.lemay@capgemini.com>
2025-08-29 17:42:08 +0200

    Init azure configuration

--- citycrisisanalyser/front/src/components/RightPanel.tsx
+++ citycrisisanalyser/front/src/components/RightPanel.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import GlassSurface from './bits/GlassSurface';
+import { useState } from 'react';
 import Accordion from './Accordion';
 
 interface RightPanelProps {
