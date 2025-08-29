6ebf2e02d6d443b0e618482e38cc9df921bd6043
clemay <clement.lemay@capgemini.com>
2025-08-29 19:50:41 +0200

    Init azure configuration

--- citycrisisanalyser/back/main_api.py
+++ citycrisisanalyser/back/main_api.py
@@ -10,4 +10,4 @@ app = FastAPI(
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("main_api:app", host="localhost", port=8000, reload=True)
+    uvicorn.run("main_api:app", host="0.0.0.0", port=8000, reload=True)
