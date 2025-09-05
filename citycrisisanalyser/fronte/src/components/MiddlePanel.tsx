67fed8a7cb41f8a7fefeab67c9758c101065e766
clemay <clement.lemay@capgemini.com>
2025-09-05 17:50:54 +0200

    Correction build POST refonte IHM

--- citycrisisanalyser/fronte/src/components/MiddlePanel.tsx
+++ citycrisisanalyser/fronte/src/components/MiddlePanel.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import '../../styles/MiddlePanel.css';
+import '../styles/MiddlePanel.css';
 
 const agentLabels = [
   'Vision analyst',
